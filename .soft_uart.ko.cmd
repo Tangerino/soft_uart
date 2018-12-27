d457a0df7b3992b2669ab09f068de8b4d355f979
Carlos Tangerino <carlos.tangerino@gmail.com>
2018-12-27 10:43:52 +0100

    select codi 1 or 2

---
+++ .soft_uart.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/dev/Desktop/soft_uart/soft_uart.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/dev/Desktop/soft_uart/soft_uart.ko /home/dev/Desktop/soft_uart/soft_uart.o /home/dev/Desktop/soft_uart/soft_uart.mod.o ;  true
---
+++ .tmp_versions/soft_uart.mod
@@ -0,0 +1,3 @@
+/home/dev/Desktop/soft_uart/soft_uart.ko
+/home/dev/Desktop/soft_uart/module.o /home/dev/Desktop/soft_uart/raspberry_soft_uart.o /home/dev/Desktop/soft_uart/queue.o
+
